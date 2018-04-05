21fdcffb7ed44f902b887e1caabe93b8ed7da430
Paislee House <pshouse13@gmail.com>
2018-04-04 21:24:34 -0500

    working on US

--- main.py
+++ main.py
@@ -21,10 +21,33 @@ def signup():
     username_error = ''
     password_error = ''
     verify_error = ''
+    email_error = ''
 
     if username = '':
         username_error = 'That is not a valid username'
+    elif len(username) < 3 or len(username) > 20:
+        username_error = 'That is not a valid username'
+    elif " " in username:
+        username_error = 'Please do not include spaces'
+
+    if password = '':
+        password_error = 'That is not a valid password'
+    elif len(password) < 3 or len(password) > 20:
+        password_error = 'That is not a valid password'
+    elif " " in password:
+        password_error = "Please do not include spaces"
+
+    if verify_password = '':
+        verify_error = 'Please provide a matching password'
+    elif verify_password != password:
+        verify_error = 'Your passwords do not match'
+
+    if email 
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', username=username)
+
     else:
-        
+        return render_template('forms.html', username=)
 
 app.run()
