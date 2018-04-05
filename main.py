b8f670898e0db011f8131b69490ddd9bd46af09d
Paislee House <pshouse13@gmail.com>
2018-04-05 08:08:23 -0500

    added css

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.config['DEBUG'] = True
 def index():
     return render_template('forms.html')
 
-@app.route("/", methods=['POST'])
+@app.route("/signup", methods=['POST'])
 def signup():
 
     username = request.form['username']
@@ -23,31 +23,35 @@ def signup():
     verify_error = ''
     email_error = ''
 
-    if username = '':
+    if username == '':
         username_error = 'That is not a valid username'
     elif len(username) < 3 or len(username) > 20:
         username_error = 'That is not a valid username'
     elif " " in username:
         username_error = 'Please do not include spaces'
 
-    if password = '':
+    if password == '':
         password_error = 'That is not a valid password'
     elif len(password) < 3 or len(password) > 20:
         password_error = 'That is not a valid password'
     elif " " in password:
-        password_error = "Please do not include spaces"
+        password_error = 'Please do not include spaces'
 
-    if verify_password = '':
+    if verify_password == '':
         verify_error = 'Please provide a matching password'
     elif verify_password != password:
         verify_error = 'Your passwords do not match'
 
-    if email 
+    if len(email) < 3 or len(email) > 20:
+        email_error = 'This is not a valid email'
+    elif " " in email:
+        email_error = 'Please do not include spaces'
 
     if not username_error and not password_error and not verify_error and not email_error:
-        return render_template('welcome.html', username=username)
+        return render_template('welcome.html', username = username)
 
     else:
-        return render_template('forms.html', username=)
+        return render_template('forms.html', username = username, password_error = password_error,
+        verify_error = verify_error, email = email, email_error = email_error)
 
 app.run()
