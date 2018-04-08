c16e290b4b85032a6b3492bf77cfd9fe1804c7ee
Paislee House <pshouse13@gmail.com>
2018-04-08 14:19:02 -0500

    finally figured out regex

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import cgi
 import os
 import jinja2
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,34 +23,29 @@ def signup():
     password_error = ''
     verify_error = ''
     email_error = ''
-
+#username validation
     if username == '':
         username_error = 'That is not a valid username'
     elif len(username) < 3 or len(username) > 20:
         username_error = 'That is not a valid username'
     elif " " in username:
         username_error = 'Please do not include spaces'
-
+#password validation
     if password == '':
         password_error = 'That is not a valid password'
     elif len(password) < 3 or len(password) > 20:
         password_error = 'That is not a valid password'
     elif " " in password:
         password_error = 'Please do not include spaces'
-
+#verify password
     if verify_password == '':
         verify_error = 'Please provide a matching password'
     elif verify_password != password:
         verify_error = 'Your passwords do not match'
-#still need to figure out email validation
-    if email == '':
-        email = ''
-    elif '@' and '.' in email:
-        email = email
-    elif len(email) < 3 or len(email) > 20:
-        email_error = 'This is not a valid email'
-    elif " " in email:
-        email_error = 'Please do not include spaces'
+#email validation using new regex method
+    if email != "": 
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            email_error = "This is not a valid email"
 
     if not username_error and not password_error and not verify_error and not email_error:
         return render_template('welcome.html', username = username)
